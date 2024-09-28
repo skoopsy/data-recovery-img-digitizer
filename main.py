6925964d0cd98d9b1eba5d4697ac137b0de971c8
Dave O <38195019+skoopsy@users.noreply.github.com>
2024-09-28 15:09:43 -0400

    Feature/01 setup basic tesseract (#1)
    * tesseract printing output for imgs in dir
    
    * added basic spell check to tesseract output
    
    * added sentence handling per img for spell check
    
    * syntax and comment cleanup

--- main.py
+++ main.py
@@ -1,7 +1,65 @@
-import cv2 # opencv-python
+import os
+
+import cv2  # opencv-python
 import pytesseract
+from spellchecker import SpellChecker
 
-img = cv2.imread("img1.png")
+input_directory = "gitexclude/example_imgs/pre-processed-1"
+img_type = ".png"
 
-custom_config = r' --oem 1 --psm 8'
-pytesseract.image_to_string(img, config=custom_config)
+# Get valid image paths from dir
+img_files = []
+for file in os.listdir(input_directory):
+    f_path = os.path.join(input_directory, file)
+
+    # Check if file as opposed to a dir
+    if os.path.isfile(f_path):
+        # Check if file has img_type extension:
+        if file.endswith(img_type):
+            print(f"Added: {file}")
+            img_files.append(f_path)
+
+        else:
+            print(f"{file} is not of type: {img_type}")
+    else:
+        print(f"{file} is not a file")
+
+print(img_files)
+
+# Load images and process with tesseract:
+print(" ")
+print("Analysing image text")
+print(20*"=")
+for f_path in img_files:
+    # Load img
+    img = cv2.imread(f_path)
+
+    # Run tesseract OCR (for printed characters)
+    tesseract_config = r''
+    tesseract_output = pytesseract.image_to_string(img, config=tesseract_config)
+    #print(f"{f_path}: {tesseract_output}")
+
+    # Strip characters from tesseract_output for spellchecker
+    #tesseract_output = tesseract_output.strip()
+
+    # Split output sentence into word list
+    tesseract_output = tesseract_output.split()
+    #print(tesseract_output)
+
+    for word in tesseract_output:
+
+        # Run spellcheck on tesseract output
+        spell = SpellChecker()  # Init spellcheck class with default word frequency list
+        spell_check_unknown = spell.unknown([word])
+        # If tesseract cannot find a word:
+        if not tesseract_output:
+            print(f"{f_path}: Cannot find a word")
+        # If word spelled correctly:
+        elif not spell_check_unknown:
+            print(f"{f_path}: Word found with a correct spelling: {word}")
+        # If word spelled incorrectly:
+        else:
+            word_correction = spell.correction(word)
+            print(f"{f_path}: Word with incorrect spelling: {word} - "
+                  f"Suggested Word: {word_correction}")
+
